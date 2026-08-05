890c5038228590fe3bc389b31e13746f7ca6a644
Danil Kolbasenko <danilvusockij@gmail.com>
2026-08-05 19:25:23 +0300

    Create style tailwindcss

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+export default function Header() {
+    return (
+        <div>
+            <header className="flex justify-between">
+                <div className="">
+                    <div>
+                        <p className="text-3xl">
+                            О нас
+                        </p>
+                        <p>
+                            Где находимся
+                        </p>
+                    </div>
+                </div>
+            </header>
+        </div>
+    )
+}
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import tailwindcss from '@tailwindcss/vite' // Импортируем плагин v4
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // Добавляем в массив плагинов
+  ],
 })
